7074067cbdcd5acb8bbdfb487708613af1996d0f
rnclee <36507023+rnclee@users.noreply.github.com>
2018-02-27 16:29:22 +0800

    Add files via upload

--- meetup_rsvp.js
+++ meetup_rsvp.js
@@ -6,9 +6,6 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 	
 var server = http.createServer(function(req, res) {
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(req.url);
-			res.end();
   if (req.url === '/login') {
 	  fs.readFile("./login.html", function (error, pgResp) {
 		if (error) {

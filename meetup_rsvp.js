73b4815f65d45d37b3fbd386991eae0e3b0621fb
rnclee <36507023+rnclee@users.noreply.github.com>
2018-02-27 15:43:02 +0800

    Add files via upload

--- meetup_rsvp.js
+++ meetup_rsvp.js
@@ -3,7 +3,7 @@ var http = require('http'),
     request = require('request');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 	
 var server = http.createServer(function(req, res) {
   if (req.url === '/login') {
